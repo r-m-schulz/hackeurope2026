4642d18f5fc16985ff06d3e6ef691fb54610557a
Robin Schulz <rsch0042@uni.sydney.edu.au>
2026-02-21 23:53:39 +0000

    Update frontend main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,6 @@
+//test deployment line
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
